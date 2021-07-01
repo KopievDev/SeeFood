64b15884b12492a4baf36ebdbd40750356e50916
Ivan Kopiev <kopiev@mustdev.ru>
2021-07-01 11:51:28 +0300

    fix

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,14 +6,74 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
-
+    
+    let imagePicker = UIImagePickerController()
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUp()
+    }
+    
+    func setUp() {
+        title = "SeeFood"
+        imagePicker.allowsEditing = false
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
     }
 
+    @IBAction func showImagePicker(sender: UIBarButtonItem) {
+        present(imagePicker, animated: true)
+    }
 
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("Error to get image from camera")
+        }
+        imageView.image = image
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("Error to make CIImage from photo")
+        }
+        detect(image: ciImage)
+        imagePicker.dismiss(animated: true)
+    }
+    
+    func detect(image: CIImage) {
+        guard let model =  try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Error to loading CoreML model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) {[weak self] request, error in
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("Error to loading results from CoreML")
+            }
+            if let firstResult = result.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self?.title = "Это Хот-Дог"
+                } else {
+                    self?.title = "Это не Хот-Дог"
+
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        
+    }
+}
